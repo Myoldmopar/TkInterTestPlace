008316c618cdfaf903f0de17c064d3eee2f13786
Edwin Lee <leeed2001@gmail.com>
2020-10-13 07:56:25 -0500

    Fix unit test issue

--- test/my_app/test_operator.py
+++ test/my_app/test_operator.py
@@ -6,7 +6,7 @@ from my_app.background_operation import BackgroundOperation
 class TestOperator(TestCase):
 
     def test_run(self):
-        b = BackgroundOperation()
+        b = BackgroundOperation(1, [])
         b.get_ready_to_go()
         self.assertFalse(b._cancel_me)
-        b.run(1)
+        b.run()
